da107dff70ada14a536bc1ff390828a2edc519dd
simeonboy12 <adekoyasimeon3@gmail.com>
2026-08-05 17:44:45 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,60 @@
+const cityInput = document.getElementById("cityInput");
+
+const cityName = document.getElementById("cityName");
+const temperature = document.getElementById("temperature");
+const condition = document.getElementById("condition");
+const wind = document.getElementById("wind");
+
+
+async function getWeather() {
+
+    const city = cityInput.value.trim();
+
+
+    if (city === "") {
+        alert("Please enter a city name");
+        return;
+    }
+
+
+    const apiKey = "YOUR_API_KEY";
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+
+    try {
+
+        const response = await fetch(url);
+
+        const data = await response.json();
+
+
+        if (data.cod === "404") {
+
+            alert("City not found");
+            return;
+
+        }
+
+
+        cityName.textContent = data.name;
+
+        temperature.textContent =
+        "Temperature: " + data.main.temp + "°C";
+
+
+        condition.textContent =
+        "Condition: " + data.weather[0].description;
+
+
+        wind.textContent =
+        "Wind: " + data.wind.speed + " m/s";
+
+
+    } catch (error) {
+
+        alert("Something went wrong");
+
+    }
+
+}
